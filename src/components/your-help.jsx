bda73518d4610164c22b0656ced93a431c7c92b3
sarib ghouri <sarib.ghouri92@gmail.com>
2025-05-31 06:01:55 -0700

    minor fix

--- src/components/your-help.jsx
+++ src/components/your-help.jsx
@@ -1,45 +1,39 @@
 
 import { Button } from "antd"
-import { ChevronRight } from "lucide-react"
+// import { ChevronRight } from "lucide-react"
 
-const YourHelp = () => {
-  return (
-    <div className="p-4">
-      <div className="bg-gradient-to-r from-[#1c0d33] via-[#371f5c] to-[#1c0d33] text-white p-6 md:p-10 flex flex-col text-center rounded-tl-[40px] md:rounded-tl-[70px] rounded-[10px] relative">
-        <div className="absolute top-4 right-4 md:top-6 md:right-6 bg-white rounded-full p-1.5">
-          <ChevronRight className="h-4 w-4 text-[#4D2D7C]" />
-        </div>
-        <h1 className="font-bold text-[20px] md:text-[22px]">We're here to help you.</h1>
-        <p className="text-[14px] md:text-[14px] mt-2 md:mt-4 text-[#dbdbdb]">
-          We're here to help you every step of the way. Whether you have questions, need assistance, or require support,
-          our dedicated team is ready to provide the solutions you need. Your satisfaction is our priority, and we're
-          committed to ensuring you have a smooth and positive experience.
-        </p>
-        <div className="flex flex-col sm:flex-row justify-center items-center text-center gap-3 sm:gap-[40px] mt-4 font-bold">
-          <div className="flex items-center gap-2">
-            <span className="text-white">•</span>
-            <span>24/7 customer support</span>
-          </div>
-          <div className="flex items-center gap-2">
-            <span className="text-white">•</span>
-            <span>Personalised service</span>
-          </div>
-          <div className="flex items-center gap-2">
-            <span className="text-white">•</span>
-            <span>Trained Professionals</span>
-          </div>
-        </div>
-        <div className="mt-6">
-          <Button
-            variant="secondary"
-            className="bg-white text-[#4D2D7C] hover:bg-gray-100 !px-8 !py-5 !font-bold rounded-lg"
-          >
-            Call at 0420 966 387
-          </Button>
-        </div>
-      </div>
-    </div>
-  )
-}
+// const YourHelp = () => {
+//   return (
+//     <div className="p-4">
+//       <div className="bg-gradient-to-r from-[#1c0d33] via-[#371f5c] to-[#1c0d33] text-white p-6 md:p-10 flex flex-col text-center rounded-tl-[40px] md:rounded-tl-[70px] rounded-[10px] relative">
+//         <div className="absolute top-4 right-4 md:top-6 md:right-6 bg-white rounded-full p-1.5">
+//           <ChevronRight className="h-4 w-4 text-[#4D2D7C]" />
+//         </div>
+//         <h1 className="font-bold text-[20px] md:text-[22px]">We're here to help you.</h1>
+//         <p className="text-[14px] md:text-[14px] mt-2 md:mt-4 text-[#dbdbdb]">
+//           We're here to help you every step of the way. Whether you have questions, need assistance, or require support,
+//           our dedicated team is ready to provide the solutions you need. Your satisfaction is our priority, and we're
+//           committed to ensuring you have a smooth and positive experience.
+//         </p>
+//         <div className="flex flex-col sm:flex-row justify-center items-center text-center gap-3 sm:gap-[40px] mt-4 font-bold">
+//           <div className="flex items-center gap-2">
+//             <span className="text-white">•</span>
+//             <span>24/7 customer support</span>
+//           </div>
+//           <div className="flex items-center gap-2">
+//             <span c
+//         </div>
+//         <div className="mt-6">
+//           <Button
+//             variant="secondary"
+//             className="bg-white text-[#4D2D7C] hover:bg-gray-100 !px-8 !py-5 !font-bold rounded-lg"
+//           >
+//             Call at 0420 966 387
+//           </Button>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default YourHelp
+// export default YourHelp
