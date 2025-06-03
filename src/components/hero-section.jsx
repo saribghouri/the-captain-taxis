b73a966f35f525e8e5a4959c92b942741de86239
saribghouri <sarib.ghouri92@gmail.com>
2025-06-03 10:27:52 -0700

    change font size

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
         <div className="w-full max-w-[90%] flex flex-col  lg:flex-row items-center">
           <div className="w-full lg:w-2/2 text-center lg:text-left">
             {/* Main heading */}
-            <h1 className="text-4xl sm:text-5xl hero-hd  md:text-4xl lg:text-[77px] xl:text-[100px] font-extrabold leading-tight">
+            <h1 className="text-4xl sm:text-5xl hero-hd  md:text-[77px] lg:text-[100px] text-[100px] font-extrabold leading-tight">
               <span className="text-black">The </span>
               <span className="text-[#4d2d7c]">
                 Captain <span className="text-black">Taxi</span>
